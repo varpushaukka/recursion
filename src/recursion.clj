6e4f5f7b9991953a30fd45f9616d5541f5528a81
Henna Kalliokoski <henna.kalliokoski@siili.com>
2019-08-06 15:23:11 +0300

    freq done

--- src/recursion.clj
+++ src/recursion.clj
@@ -87,31 +87,62 @@
     ))
 
 (defn power [n k]
-  :-)
+  (if (zero? k) 
+    1
+    (* n (power n (dec k)))
+    ))
+
 
 (defn fib [n]
-  :-)
+  (if (zero? n) 
+    0
+    (if (= 1 n)
+      1
+      (+ (fib (dec n)) (fib (- n 2))))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (< how-many-times 1)
+    '()
+    (conj (my-repeat (dec how-many-times) what-to-repeat) what-to-repeat)))
 
 (defn my-range [up-to]
-  [:-])
+  (if (zero? up-to)
+    '()
+    (conj (my-range (dec up-to)) (dec up-to))
+    ))
 
 (defn tails [a-seq]
-  [:-])
-
+  (if (empty? a-seq)
+    (cons [] a-seq)
+    (cons a-seq (tails (rest a-seq)))))
+  
 (defn inits [a-seq]
-  [:-])
+  (cons [] 
+        (map (fn init [x] 
+               (if (zero? x)
+                 []
+                 (conj (init (dec x)) x)))
+             a-seq)))
 
 (defn rotations [a-seq]
-  [:-])
-
+  (if (empty? a-seq)
+    [[]]
+    (loop [result []
+           r (count a-seq)]
+      (if (zero? r) 
+        result
+        (recur (conj result (concat (drop r a-seq) (take r a-seq))) (dec r))))))
+  
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq) 
+    freqs
+    (let [fr (first a-seq)]
+      (if (get freqs fr)
+        (my-frequencies-helper (assoc freqs fr (inc (get freqs fr))) (rest a-seq))
+        (my-frequencies-helper (assoc freqs (first a-seq) 1) (rest a-seq))))))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
   [:-])
